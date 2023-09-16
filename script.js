9f21d66b63f9a355d4a8a7877d21473a5d93009f
Mariusz-Porowski <126398966+Mariusz-Porowski@users.noreply.github.com>
2023-09-16 17:49:50 +0200

    Norway
    bacz

---
+++ script.js
@@ -0,0 +1,37 @@
+const ball = document.querySelector('img')
+const input = document.querySelector('input')
+const answer = document.querySelector('.answer')
+const error = document.querySelector('.error')
+
+const answersArr = ['Tak!', 'Nie.', 'Moze.', 'Cięzko powiedzieć.', 'Nie chcesz znać odpowiedzi na to pytanie..']
+
+// FUNKCJE
+const showAnimation = () => {
+	ball.classList.add('shake-animation')
+	setTimeout(checkInput, 1000)
+}
+
+const checkInput = () => {
+	if (input.value !== '' && input.value.slice(-1) === '?') {
+		generateAnswer()
+		error.textContent = ''
+		ball.classList.remove('shake-animation')
+	} else if (input.value !== '' && input.value.slice(-1) !== '?') {
+		error.textContent = 'Odpowiedź musi zawierać na końcu zdania znak "?".'
+		answer.textContent = ''
+		ball.classList.remove('shake-animation')
+	} else {
+		error.textContent = 'Musisz zadać jakieś pytanie mordo.'
+		answer.textContent = ''
+		ball.classList.remove('shake-animation')
+	}
+}
+
+const generateAnswer = () => {
+	const number = Math.floor(Math.random() * 5)
+	answer.innerHTML = `<span>Odpowiedź:</span> ${answersArr[number]}`
+
+	console.log(number)
+}
+
+ball.addEventListener('click', showAnimation)
